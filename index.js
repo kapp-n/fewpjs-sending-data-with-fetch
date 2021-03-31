e0ac48754412e61a78c3be6394f8882cda24c7f1
Mollie Dickenson <molliedickenson@gmail.com>
2021-03-31 14:56:20 -0400

    commit tree 0893d31f1d860e704b87e530b0ef6b62cd1ae5b1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const body = document.querySelector('body')
+
+
+function submitData(userName, userEmail){
+    const formData = {
+        name: userName, 
+        email: userEmail
+    }
+    const postObj = { 
+        method: 'POST',
+        headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+        body: JSON.stringify(formData)  
+    }
+     return fetch('http://localhost:3000/users', postObj)
+    .then(response => response.json())
+    .then(data =>
+        document.body.innerHTML = data['id'])
+    .catch((error) => {
+        document.body.innerHTML = error.message
+        
+    })
+}
+
+      
